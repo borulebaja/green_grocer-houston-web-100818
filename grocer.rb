cef50735ad6ebc75c550c3cbdaf513ddb81c176e
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-10-06 05:21:08 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,15 @@
 def consolidate_cart(cart)
-  # code here
+  hash = {}
+  cart.each do |item_hash|
+    item_hash.each do |name, price_hash|
+      if hash[name].nil?
+        hash[name] = price_hash.merge({:count => 1})
+      else
+        hash[name][:count] += 1
+      end
+    end
+  end
+  hash
 end
 
 def apply_coupons(cart, coupons)
