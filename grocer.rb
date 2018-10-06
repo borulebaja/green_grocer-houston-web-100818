69ea864b2afdc9ca93c719cbb892c385a6224e4a
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-10-06 05:45:40 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -12,9 +12,11 @@ def consolidate_cart(cart)
   hash
 end
 
+
 def apply_coupons(cart, coupons)
   
 end
+
 
 def apply_clearance(cart)
   cart.each do |item, price_hash|
@@ -27,16 +29,6 @@ end
 
 
 def checkout(cart, coupons)
-  cart = consolidate_cart(items)
-  cart1 = apply_coupons(cart, coupons)
-  cart2 = apply_clearance(cart1)
-  
-  total = 0
-  
-  cart2.each do |name, price_hash|
-    total += price_hash[:price] * price_hash[:count]
-  end
-  
-  total > 100 ? total * 0.9 : total
+ 
   
 end
